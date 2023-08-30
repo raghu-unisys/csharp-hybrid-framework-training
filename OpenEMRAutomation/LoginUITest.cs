c2f08fe8a16568140ff6bc5b535b077b4c987b0b
R <raghunathan.r@unisys.com>
2023-08-30 20:08:08 +0530

    added setup and teardown

--- OpenEMRAutomation/LoginUITest.cs
+++ OpenEMRAutomation/LoginUITest.cs
@@ -10,23 +10,42 @@ namespace Unisys.OpenEMRAutomation
 {
     public class LoginUITest
     {
+        // we make the driver as an instance varibale so that it can be accessed across all methods
+        IWebDriver driver;
+
+        // setup attribute runs before every test. So in this we add the code to open the browser thus reducing repeated code
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Url = "https://demo.openemr.io/b/openemr";
+        }
+
+        // Teardown method runs after every test case. We can use this to quite the browser
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
         [Test]
         public void ValidateTitleTest() 
         { 
-            IWebDriver driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-
-            driver.Url = "https://demo.openemr.io/b/openemr";
-
             string actualTitle = driver.Title;
             Console.WriteLine(actualTitle);
 
             // validating the title of the webpage
-            Assert.AreEqual("OpenEMR Login", actualTitle);
+            Assert.That(actualTitle, Is.EqualTo("OpenEMR Login"));
 
+        }
 
-            driver.Quit();
+        [Test]
+        public void ValidatePlaceholderTest()
+        {
+            string actualUsernamePlaceholder = driver.FindElement(By.Id("authUser")).GetAttribute("placeholder");
+            Assert.That(actualUsernamePlaceholder, Is.EqualTo("Username"));
         }
     }
 }
