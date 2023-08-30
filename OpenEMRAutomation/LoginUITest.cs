417fb490e3035291520063f84085913e7946cece
R <raghunathan.r@unisys.com>
2023-08-30 19:48:02 +0530

    added title test

---
+++ OpenEMRAutomation/LoginUITest.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unisys.OpenEMRAutomation
+{
+    public class LoginUITest
+    {
+        [Test]
+        public void ValidateTitleTest() 
+        { 
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            driver.Url = "https://demo.openemr.io/b/openemr";
+
+            string actualTitle = driver.Title;
+            Console.WriteLine(actualTitle);
+
+            // validating the title of the webpage
+            Assert.AreEqual("OpenEMR Login", actualTitle);
+
+
+            driver.Quit();
+        }
+    }
+}
