2f229104af89ba58fd3eacd4bd5517a747227c80
R <raghunathan.r@unisys.com>
2023-08-31 10:56:04 +0530

    added data driven test framework for valid login test

--- OpenEMRAutomation/LoginTest.cs
+++ OpenEMRAutomation/LoginTest.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Unisys.Base;
+using OpenQA.Selenium.Support.UI;
+using NuGet.Frameworks;
 
 namespace Unisys.OpenEMRAutomation
 {
@@ -13,11 +15,22 @@ namespace Unisys.OpenEMRAutomation
     public class LoginTest : AutomationWrapper
     {
 
+        // TestCase will considered as an induvidual test method. This is called as Data driven Framework
         [Test]
-        public void ValidLoginTest()
+        [TestCase("admin", "pass", "English (Indian)", "OpenEMR")]
+        [TestCase("physician", "physician", "English (Indian)", "OpenEMR")]
+        public void ValidLoginTest(string username, string password, string language, string expectedTitle)
         {
-            driver.FindElement(By.Id("authUser")).SendKeys("admin");
-            driver.FindElement(By.Id("clearPass")).SendKeys("admin");
+            driver.FindElement(By.Id("authUser")).SendKeys(username);
+            driver.FindElement(By.Id("clearPass")).SendKeys(password);
+            SelectElement selectLanguage = new SelectElement(driver.FindElement(By.XPath("//select[@name=\"languageChoice\"]")));
+            selectLanguage.SelectByText(language);
+            driver.FindElement(By.Id("login-button")).Click();
+
+            // waiting for page load to complete
+
+            // validating the window tite
+            Assert.That(driver.Title, Is.EqualTo(expectedTitle));
 
         }
     }
